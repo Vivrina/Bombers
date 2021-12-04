7f346894eff27f9f2960adc1b1d96f861e38a15e
Vivrina <i_videeva@mail.ru>
2021-12-05 02:10:42 +0300

    Initial commit

---
+++ src/main/java/controller/MainController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+
+    @FXML
+    public Button mainButton;
+    // buttonOk.graphicProperty().setValue(new ImageView(imageOk));
+
+
+
+
+
+
+
+
+//    @FXML
+//    private void buttonClicked() {
+//        mainButton.gr
+//        mainButton.setText("Click me again!");
+//    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
