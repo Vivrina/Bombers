68cc72a08a91126bbc2a6291d29fecc128c81f4d
Arthur <rthoor@mail.ru>
2021-12-22 07:31:15 +0300

    ura

--- src/main/java/controller/EndController.java
+++ src/main/java/controller/EndController.java
@@ -23,23 +23,23 @@ public class EndController implements Initializable {
     @FXML
     public ImageView resultBG;
 
-    public void toTheMenu(ActionEvent event) {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Main.fxml"));
-        Node node=(Node) event.getSource();
-        Stage stage=(Stage) node.getScene().getWindow();
-        Parent root = null;
-        try {
-            root = fxmlLoader.load();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.setMaximized(true);
-        stage.setFullScreen(true);
-        stage.show();
-    }
+//    public void toTheMenu(ActionEvent event) {
+//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Main.fxml"));
+//        Node node=(Node) event.getSource();
+//        Stage stage=(Stage) node.getScene().getWindow();
+//        Parent root = null;
+//        try {
+//            root = fxmlLoader.load();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        Scene scene = new Scene(root);
+//        stage.setScene(scene);
+//        stage.setMaximized(true);
+//        stage.setFullScreen(true);
+//        stage.show();
+//    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -54,7 +54,6 @@ public class EndController implements Initializable {
     void changeBG(String bg){
         try {
             resultBG.setImage(new Image(new FileInputStream(bg)));
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
